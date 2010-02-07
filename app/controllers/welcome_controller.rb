e1e56e0197b1b30c380619a08004d279e89656a0
Paul Betts <paul@paulbetts.org>
2010-02-06 21:23:15 -0800

    Switch to using ListModel class

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,9 @@ class WelcomeController < ApplicationController
   acts_as_iphone_controller :test_mode => true
 
   def index
-    item = ListItem.new
+    item = ListModel.new
     item.url = "http://www.google.com"
     item.caption = render_to_string(:partial => "tile", :locals => { :image_url => "http://knowyourmeme.com/i/984/original/1238805246000.jpg" })
     @items = [item]
   end
-end
-
-class ListItem
-  attr_accessor :url
-  attr_accessor :caption
 end
