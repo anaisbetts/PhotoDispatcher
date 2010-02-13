10dc13fa9a3d8c8f7f7bdb4e4a08345cd75d949f
Paul Betts <paul@paulbetts.org>
2010-02-12 22:52:10 -1000

    Add Facebook uploader

---
+++ app/actions/facebook_action.rb
@@ -0,0 +1,15 @@
+class EvernoteAction < MailAction
+  def width; 1024; end
+  def height; 768; end
+
+  def email_address(options = {})
+    photo = options[:item]
+    photo.user.settings[:facebook_email]
+  end
+
+  def friendly_name
+    "Upload to Facebook"
+  end
+
+  register_as_action
+end
